939a2e07485c9e6dae69aaec1ad3caf463620049
dmitr2ish <dmitr2ish@yandex.ru>
2020-07-11 19:26:54 +0500

    - deleted all controllers and added only view controllers/

---
+++ src/main/java/com/contoller/view/AdminController.java
@@ -0,0 +1,16 @@
+package com.contoller.view;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/admin/*")
+public class AdminController {
+
+    @GetMapping
+    public ModelAndView listPage() {
+        return new ModelAndView("admin/list");
+    }
+}
---
+++ src/main/java/com/contoller/view/UserController.java
@@ -0,0 +1,16 @@
+package com.contoller.view;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    @GetMapping
+    public ModelAndView userPage() {
+        return new ModelAndView("user/user");
+    }
+}
